9bc14e48915be40e9db4ecc327f4a36d36d898eb
syr <sufr3b9@foxmail.com>
2024-07-14 01:42:17 +0800

    5

--- src/script.js
+++ src/script.js
@@ -29,7 +29,7 @@ const square = {
 const bottomLeftSquare = {
     x: 0,
     y: 0, // Will be adjusted in resizeCanvas
-    width: 135*1.5,
+    width: 145*1.5,
     height: 120*1.5,
     color: 'red'
 };
@@ -57,7 +57,7 @@ class Ball {
     }
 
     draw() {
-        const size = this.radius * 2 * 2.2*1.5; // Increased size
+        const size = this.radius * 2 * 2.*1.5; // Increased size
         ctx.drawImage(ballImage, this.x - size / 2, this.y - size / 2, size, size);
     }
 
